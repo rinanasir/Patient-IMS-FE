e090f0d83ff8ad10e58ed4b9276b638c835a36d1
rinanasir <rinanasir25@gmail.com>
2022-10-03 21:18:53 +0600

    structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { HomeComponent } from './home/home.component';
+import { AddPatientComponent } from './Patient/add-patient/add-patient.component';
+import { ListPatientComponent } from './Patient/list-patient/list-patient.component';
+import { EditPatientComponent } from './Patient/edit-patient/edit-patient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    HomeComponent,
+    AddPatientComponent,
+    ListPatientComponent,
+    EditPatientComponent
   ],
   imports: [
     BrowserModule,
