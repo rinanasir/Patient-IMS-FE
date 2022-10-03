c9f24f63a2511b428d35d104bdad33465e078bcf
rinanasir <rinanasir25@gmail.com>
2022-10-03 21:48:48 +0600

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AddPatientComponent } from './Patient/add-patient/add-patient.component';
+import { EditPatientComponent } from './Patient/edit-patient/edit-patient.component';
+import { ListPatientComponent } from './Patient/list-patient/list-patient.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "patients", component: ListPatientComponent },
+  { path: "addPatient", component: AddPatientComponent },
+  { path: "editPatient", component: EditPatientComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-navigation-bar',
+  selector: 'Navigation-Bar',
   templateUrl: './navigation-bar.component.html',
   styleUrls: ['./navigation-bar.component.css']
 })
